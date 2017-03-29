7a8858538e7ef80531180635d6ab91f591de3278
Paul <pjuliano@gmail.com>
2017-03-29 13:54:26 -0400

    Added code to calculate IMPL sales from last year (bodies only).

--- New Customer Proof Worksheet.sql
+++ New Customer Proof Worksheet.sql
@@ -8,7 +8,17 @@ From
 Where 
   Extract(Year From A.Invoicedate) = Extract(Year From Sysdate) -1 And 
   A.Part_Product_Family In ('COMM','GNSIS','PRIMA','RENOV','RESTO','STAGE','SUST','TEFGE','XP1','TRINX','EXHEX','EXORL','OCT','ZMAX','LODI','OTMED') And
-  A.Customer_No = '1020';
+  A.Customer_No = 'A13430';
+--Check to see if they ordered implants last year even though that's not part of the contest rules.
+Select 
+  Sum(A.Allamounts) 
+From 
+  Kd_Sales_Data_Test A 
+Where 
+  Extract(Year From A.Invoicedate) = Extract(Year From Sysdate) -1 And 
+  A.Part_Product_Family In ('COMM','GNSIS','PRIMA','RENOV','RESTO','STAGE','SUST','TEFGE','XP1','TRINX','EXHEX','EXORL','OCT','ZMAX','LODI','OTMED') And
+  A.PArt_Product_Code = 'IMPL' And
+  A.Customer_No = 'A13430';
 --Check that sum of this year's sales is >= 5000.
 Select
   Sum(A.Allamounts)
@@ -17,7 +27,7 @@ From
 Where
   Extract(Year From A.Invoicedate) = Extract(Year From Sysdate) And
   A.Charge_Type = 'Parts' And
-  A.Customer_No = '23214';
+  A.Customer_No = '2944';
 --Check that sum of Implant Bodies sales is <= 3750.
 Select
   Sum(A.Allamounts)
