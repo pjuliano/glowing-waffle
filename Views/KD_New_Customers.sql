d2bf88d21ad8cb6536fda0f29ee194f9afad527a
Paul <pjuliano@gmail.com>
2017-03-31 12:05:51 -0400

    Changed criteria from: 	<= $1000 Last Year to: 	<= $1000 in bodies over last 2 years
    per Mike, Steve, and Deb.

--- Views/KD_New_Customers.sql
+++ Views/KD_New_Customers.sql
@@ -1,4 +1,4 @@
---Create Or Replace View KD_New_Customers As
+Create Or Replace View KD_New_Customers As
 Select
     A.Salesman_Code,
     C.Name As Salesman_Name,
@@ -8,52 +8,7 @@ Select
     Sum(Case 
           When
             Extract (Year From A.Invoicedate) Between Extract(Year From Sysdate) -2 And Extract(Year From Sysdate) -1 And
-            A.Part_Product_Code = 'IMPL' And
-            A.Catalog_No Not In ('MAX-8-11',
-                                 'MAX-9-11',
-                                 'MAX-7-11',
-                                 'MAX-7-7',
-                                 'MAX-7-9',
-                                 'MAX-8-7',
-                                 'MAX-8-9',
-                                 'MAX-9-7',
-                                 'MAX-9-9',
-                                 'MAXIT7-11',
-                                 'MAXIT7-7',
-                                 'MAXIT7-9',
-                                 'MAXIT8-11',
-                                 'MAXIT8-7',
-                                 'MAXIT8-9',
-                                 'MAXIT9-11',
-                                 'MAXIT9-7',
-                                 'MAXIT9-9',
-                                 '15707K',
-                                 '15705K',
-                                 '15706K',
-                                 '15710K',
-                                 '15708K',
-                                 '15709K',
-                                 '15713K',
-                                 '15711K',
-                                 '15712K',
-                                 'TRI-MAX7-11',
-                                 'TRI-MAX7-7',
-                                 'TRI-MAX7-9',
-                                 'TRI-MAX8-11',
-                                 'TRI-MAX8-7',
-                                 'TRI-MAX8-9',
-                                 'TRI-MAX9-11',
-                                 'TRI-MAX9-7',
-                                 'TRI-MAX9-9',
-                                 'Z-MAX7-11',
-                                 'Z-MAX7-7',
-                                 'Z-MAX7-9',
-                                 'Z-MAX8-11',
-                                 'Z-MAX8-7',
-                                 'Z-MAX8-9',
-                                 'Z-MAX9-11',
-                                 'Z-MAX9-7',
-                                 'Z-MAX9-9')
+            A.Part_Product_Code = 'IMPL'
           Then
             A.Allamounts
           Else
@@ -174,52 +129,7 @@ Having
   Sum(Case 
         When
           Extract (Year From A.Invoicedate) Between Extract(Year From Sysdate) -2 And Extract(Year From Sysdate) -1 And
-          A.Part_Product_Code = 'IMPL' And
-            A.Catalog_No Not In ('MAX-8-11',
-                                 'MAX-9-11',
-                                 'MAX-7-11',
-                                 'MAX-7-7',
-                                 'MAX-7-9',
-                                 'MAX-8-7',
-                                 'MAX-8-9',
-                                 'MAX-9-7',
-                                 'MAX-9-9',
-                                 'MAXIT7-11',
-                                 'MAXIT7-7',
-                                 'MAXIT7-9',
-                                 'MAXIT8-11',
-                                 'MAXIT8-7',
-                                 'MAXIT8-9',
-                                 'MAXIT9-11',
-                                 'MAXIT9-7',
-                                 'MAXIT9-9',
-                                 '15707K',
-                                 '15705K',
-                                 '15706K',
-                                 '15710K',
-                                 '15708K',
-                                 '15709K',
-                                 '15713K',
-                                 '15711K',
-                                 '15712K',
-                                 'TRI-MAX7-11',
-                                 'TRI-MAX7-7',
-                                 'TRI-MAX7-9',
-                                 'TRI-MAX8-11',
-                                 'TRI-MAX8-7',
-                                 'TRI-MAX8-9',
-                                 'TRI-MAX9-11',
-                                 'TRI-MAX9-7',
-                                 'TRI-MAX9-9',
-                                 'Z-MAX7-11',
-                                 'Z-MAX7-7',
-                                 'Z-MAX7-9',
-                                 'Z-MAX8-11',
-                                 'Z-MAX8-7',
-                                 'Z-MAX8-9',
-                                 'Z-MAX9-11',
-                                 'Z-MAX9-7',
-                                 'Z-MAX9-9')
+          A.Part_Product_Code = 'IMPL'
         Then
           A.Allamounts
         Else
