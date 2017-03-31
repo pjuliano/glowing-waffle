d2defecb16cf331e6feedf018c2b38b6e9f3e5e8
Paul <pjuliano@gmail.com>
2017-03-31 12:06:47 -0400

    Added Nullif() wrappers around every divisor.

--- Work In Progress/WWSS Rework Freight.sql
+++ Work In Progress/WWSS Rework Freight.sql
@@ -47,7 +47,7 @@ Select
                        Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                   Then A.Allamounts
                   Else 0
-             End)) / (C.Total_Sales_Days - C.Elapsed_Work_Days),2) As Month_Daily_Required,
+             End)) / NullIf((C.Total_Sales_Days - C.Elapsed_Work_Days),0),2) As Month_Daily_Required,
   Sum(Case When Decode(Extract(Month From Sysdate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') = 
                 Decode(Extract(Month From A.Invoicedate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') And
                 Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
@@ -60,7 +60,7 @@ Select
                        Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                   Then A.Allamounts
                   Else 0
-             End) / Decode(Extract(Month From Sysdate),1,B.Q1,2,B.Q1,3,B.Q1,4,B.Q2,5,B.Q2,6,B.Q2,7,B.Q3,8,B.Q3,9,B.Q3,10,B.Q4,11,B.Q4,12,B.Q4)) * 100,2) As Qtr_Pct_Plan,
+             End) / NullIf(Decode(Extract(Month From Sysdate),1,B.Q1,2,B.Q1,3,B.Q1,4,B.Q2,5,B.Q2,6,B.Q2,7,B.Q3,8,B.Q3,9,B.Q3,10,B.Q4,11,B.Q4,12,B.Q4),0)) * 100,2) As Qtr_Pct_Plan,
   Round(Decode(Extract(Month From Sysdate),1,B.Q1,2,B.Q1,3,B.Q1,4,B.Q2,5,B.Q2,6,B.Q2,7,B.Q3,8,B.Q3,9,B.Q3,10,B.Q4,11,B.Q4,12,B.Q4) - 
         Sum(Case When Decode(Extract(Month From Sysdate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') = 
                       Decode(Extract(Month From A.Invoicedate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') And
@@ -74,19 +74,19 @@ Select
                        Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                 Then A.Allamounts
                 Else 0
-              End)) / (D.Total_Sales_Days - D.Elapsed_Work_Days),2) As Qtr_Daily_Required,
+              End)) / NullIf((D.Total_Sales_Days - D.Elapsed_Work_Days),0),2) As Qtr_Daily_Required,
   Round(((Sum(Case When Decode(Extract(Month From Sysdate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') = 
                         Decode(Extract(Month From A.Invoicedate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') And
                         Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                    Then A.Allamounts
                    Else 0
-              End) / D.Elapsed_Work_Days) * D.Total_Sales_Days) / Decode(Extract(Month From Sysdate),1,B.Q1,2,B.Q1,3,B.Q1,4,B.Q2,5,B.Q2,6,B.Q2,7,B.Q3,8,B.Q3,9,B.Q3,10,B.Q4,11,B.Q4,12,B.Q4) * 100,2) As Qtr_Run_Rate_Pct,
+              End) / NullIf(D.Elapsed_Work_Days,0)) * D.Total_Sales_Days) / NullIf(Decode(Extract(Month From Sysdate),1,B.Q1,2,B.Q1,3,B.Q1,4,B.Q2,5,B.Q2,6,B.Q2,7,B.Q3,8,B.Q3,9,B.Q3,10,B.Q4,11,B.Q4,12,B.Q4),0) * 100,2) As Qtr_Run_Rate_Pct,
   Round(((Sum(Case When Decode(Extract(Month From Sysdate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') = 
                         Decode(Extract(Month From A.Invoicedate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') And
                         Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                    Then A.Allamounts
                    Else 0
-              End) / D.Elapsed_Work_Days) * D.Total_Sales_Days),2) As Qtr_Run_Rate_Sales,
+              End) / NullIf(D.Elapsed_Work_Days,0)) * D.Total_Sales_Days),2) As Qtr_Run_Rate_Sales,
   Sum(Case When Extract(Year From A.InvoiceDate) = Extract(Year From Sysdate)
            Then A.Allamounts
            Else 0
@@ -95,7 +95,7 @@ Select
   Round((Sum(Case When Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                   Then A.Allamounts
                   Else 0
-             End) / B.Total) * 100,2) As Year_Pct_Plan,
+             End) / NullIf(B.Total,0)) * 100,2) As Year_Pct_Plan,
   Round(Sum(Case When Extract(Month From A.Invoicedate) In (10,11,12) And
                       Extract(Month From Sysdate) In (1,2,3) And
                       Extract(Year From A.Invoicedate) = Extract(Year From Sysdate) -1
@@ -116,7 +116,7 @@ Select
   Round((Sum(Case When Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                   Then A.Allamounts
                   Else 0
-             End) / 
+             End) / NullIf(
          Sum(Case When Extract(Month From A.Invoicedate) In (10,11,12) And
                        Extract(Month From Sysdate) In (1,2,3) And
                        Extract(Year From A.Invoicedate) = Extract(Year From Sysdate) -1
@@ -133,7 +133,7 @@ Select
                        Extract(Month From Sysdate) In (10,11,12) And
                        Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                   Then A.Allamounts
-             End)) * 100,2) As Prior_Qtr_Pct,
+             End),0)) * 100,2) As Prior_Qtr_Pct,
   Sum(Case When Decode(Extract(Month From Sysdate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') = 
                 Decode(Extract(Month From A.Invoicedate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') And
                 A.InvoiceDate <= To_Date(Extract(Month From Sysdate) || '/' || Extract(Day From Sysdate) || '/' || (Extract(Year From Sysdate) -1),'MM/DD/YYYY')
@@ -145,13 +145,13 @@ Select
                        Extract(Year From A.Invoicedate) = Extract(Year From Sysdate)
                   Then A.Allamounts
                   Else 0
-             End) / 
+             End) / NullIf(
            Sum(Case When Decode(Extract(Month From Sysdate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') = 
                          Decode(Extract(Month From A.Invoicedate),1,'QTR1',2,'QTR1',3,'QTR1',4,'QTR2',5,'QTR2',6,'QTR2',7,'QTR3',8,'QTR3',9,'QTR3',10,'QTR4',11,'QTR4',12,'QTR4') And
                          A.InvoiceDate <= To_Date(Extract(Month From Sysdate) || '/' || Extract(Day From Sysdate) || '/' || (Extract(Year From Sysdate) -1),'MM/DD/YYYY')
                     Then A.Allamounts
                     Else 0
-               End)) * 100,2) As Prior_Year_Qtd_Pct,
+               End),0)) * 100,2) As Prior_Year_Qtd_Pct,
   Round(Sum(Case When A.Invoicedate <= To_Date(Extract(Month From Sysdate) || '/' || Extract(Day From Sysdate) || '/' || (Extract(Year From Sysdate) -1) ,'MM/DD/YYYY')
                  Then A.Allamounts
                  Else 0
@@ -159,11 +159,11 @@ Select
 Round((Sum(Case When Extract(Year From A.InvoiceDate) = Extract(Year From Sysdate)
                 Then A.Allamounts
                 Else 0
-           End) /
+           End) / NullIf(
        Sum(Case When A.Invoicedate <= To_Date(Extract(Month From Sysdate) || '/' || Extract(Day From Sysdate) || '/' || (Extract(Year From Sysdate) -1) ,'MM/DD/YYYY')
                  Then A.Allamounts
                  Else 0
-            End)) * 100,2) As Prior_YTD_Pct   
+            End),0)) * 100,2) As Prior_YTD_Pct   
 From
   Quota_Dist B Full Outer Join Kd_Sales_Data_Request A
     On B.F1 = A.Corporate_Form,
