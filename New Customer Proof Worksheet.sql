b3ab1d64bf1c485258746a57a5bc80774a2b675f
Paul <pjuliano@gmail.com>
2017-04-10 16:34:01 -0400

    Added IMPL restriction in Where clause for last two year sales summary.

--- New Customer Proof Worksheet.sql
+++ New Customer Proof Worksheet.sql
@@ -1,15 +1,15 @@
 --Find the account.
 Select * From Customer_Info A Where A.Name Like '%Weaks%';
 --Check last year's sales in Implant product families is <= 1000.
-Select 
+Select
   Sum(A.Allamounts) 
 From 
   Kd_Sales_Data_Test A 
 Where 
-  Extract(Year From A.Invoicedate) = Extract(Year From Sysdate) -1 And 
+  Extract(Year From A.Invoicedate) In (2015,2016) And
   A.Part_Product_Family In ('COMM','GNSIS','PRIMA','RENOV','RESTO','STAGE','SUST','TEFGE','XP1','TRINX','EXHEX','EXORL','OCT','ZMAX','LODI','OTMED') And
   A.Part_Product_Code = 'IMPL' And
-  A.Customer_No = '27682';
+  A.Customer_No = '1020';
 --Check that sum of this year's sales is >= 5000.
 Select
   Sum(A.Allamounts)
@@ -18,14 +18,18 @@ From
 Where
   Extract(Year From A.Invoicedate) = Extract(Year From Sysdate) And
   A.Charge_Type = 'Parts' And
-  A.Customer_No = '27682';
+  A.Part_Product_Family In ('COMM','GNSIS','PRIMA','RENOV','RESTO','STAGE','SUST','TEFGE','XP1','TRINX','EXHEX','EXORL','OCT','ZMAX','LODI','OTMED') And
+  A.Customer_No = '23214';
 --Check that sum of Implant Bodies sales is <= 3750.
 Select
+  Catalog_Desc,
   Sum(A.Allamounts)
 From
   Kd_Sales_Data_Test A
 Where
   Extract(Year From A.Invoicedate) = Extract(Year From Sysdate) And
   A.Part_Product_Code = 'IMPL' And
-  A.Customer_No = '27682';
+  A.Customer_No = '1020'
+Group By
+  Catalog_Desc;
 --Bask in the glory that your code is superior and always right. 
