4440cd28a8932890ee93499791de3ffb5668b648
Paul <pjuliano@gmail.com>
2017-04-17 14:50:04 -0400

    Added some fields.

--- Data Requests/Customer Service/National Account Sales History.sql
+++ Data Requests/Customer Service/National Account Sales History.sql
@@ -1,5 +1,6 @@
 Select
-  A.InvoiceDate,
+  A.Customer_Name,
+  A.Invoicedate,
   A.Catalog_No,
   A.Catalog_Desc,
   A.Invoiced_Qty,
@@ -10,3 +11,6 @@ Where
   A.Association_No = 'N1035' And
   Extract(Year From A.Invoicedate) = 2016 And
   A.Charge_Type = 'Parts'
+Order By
+  A.Customer_Name,
+  A.Invoicedate
