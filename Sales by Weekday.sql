6f9a9175d75c20a5ecf9c00300b3d240079d8d4d
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-05-22 17:22:05 -0400

    Sales by weekday... interesting.

---
+++ Sales by Weekday.sql
@@ -0,0 +1,21 @@
+Select
+  Person_Info_Api.Get_Name(A.Salesman_Code) As Rep,
+  A.Region_Code,
+  A.Invoicedate,
+  Calendar_Api.Get_Week_Day(A.InvoiceDate) As Day,
+  Sum(A.AllAmounts)
+From
+  Kd_Sales_Data_Request A
+Where
+  A.Charge_Type = 'Parts' And
+  A.Corporate_Form = 'DOMDIR' And
+  A.Invoicedate >= To_Date('01/01/2016','MM/DD/YYYY')
+Group By
+  Person_Info_Api.Get_Name(A.Salesman_Code),
+  A.Region_Code,
+  A.Invoicedate,
+  Calendar_Api.Get_Week_Day(A.InvoiceDate)
+Order By
+  A.Invoicedate,
+  A.Region_Code,
+  Person_Info_Api.Get_Name(A.Salesman_Code)
