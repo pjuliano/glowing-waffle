5c046d00e877838ba303496245b23221b1dfc94f
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-05-26 13:54:20 -0400

    Interesting data set when graphed and plotted against total quota divided by sales days this year.

--- Sales by Weekday.sql
+++ Sales by Weekday.sql
@@ -2,14 +2,14 @@ Select
   Person_Info_Api.Get_Name(A.Salesman_Code) As Rep,
   A.Region_Code,
   A.Invoicedate,
-  Calendar_Api.Get_Week_Day(A.InvoiceDate) As Day,
-  Sum(A.AllAmounts)
+  Calendar_Api.Get_Week_Day(A.Invoicedate) As Day,
+  Sum(A.AllAmounts) As Total
 From
   Kd_Sales_Data_Request A
 Where
   A.Charge_Type = 'Parts' And
   A.Corporate_Form = 'DOMDIR' And
-  A.Invoicedate >= To_Date('01/01/2016','MM/DD/YYYY')
+  A.Invoicedate >= To_Date('01/01/2017','MM/DD/YYYY')
 Group By
   Person_Info_Api.Get_Name(A.Salesman_Code),
   A.Region_Code,
