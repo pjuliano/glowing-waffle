fe64ee226632291feda0718eba9b138f95a5cf6d
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-06-07 12:17:41 -0400

    New view per Mike Nealon's request.

---
+++ Views/KD_Todays_EU_Vouchers.sql
@@ -0,0 +1,19 @@
+Create or Replace View KD_TODAYS_EU_VOUCHERS As
+Select
+  A.Company,
+  A.Voucher_Date,
+  A.Accounting_Year,
+  A.Accounting_Period,
+  A.Userid,
+  A.Entered_By_User_Group,
+  A.Account,
+  A.Account_Desc,
+  A.Debet_Amount As Debit_Amount,
+  A.Credit_Amount,
+  A.Amount,
+  A.Text
+From
+  Gen_Led_Voucher_Row_Union_Qry A
+Where
+  --A.Company != '100' And
+  A.Voucher_Date = Trunc(Sysdate);
