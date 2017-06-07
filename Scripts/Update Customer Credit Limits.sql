4f8e861c933756a91d392fcb77775913be586b80
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-06-07 12:17:27 -0400

    Modified script after Tobi requested another update. Added failed/success tag logic.

--- Scripts/Update Customer Credit Limits.sql
+++ Scripts/Update Customer Credit Limits.sql
@@ -6,9 +6,27 @@ Declare
    E_ Varchar2(32000) := 'DO'; --p4
 Begin
   For CL_Update In (Select
-                      *
+                      A.Customer_Id,
+                      A.New_Credit_Limit,
+                      A.Status,
+                      B.ObjID,
+                      B.Objversion
                     From
-                      Kd_Credit_Limit_Update A)
-                      
-  Ifsapp.Customer_Credit_Info_Api.Modify__( A_ , B_ , C_ , D_ , E_ );
+                      Kd_Credit_Limit_Update A,
+                      Customer_Credit_Info B
+                    Where
+                      A.Customer_Id = B.Identity And
+                      A.Status = 'NEW')
+  Loop 
+    A_ := Null;
+    B_ := Cl_Update.Objid;
+    C_ := Cl_Update.Objversion;
+    D_ := 'CREDIT_LIMIT'||Chr(31)|| Cl_Update.New_Credit_Limit ||Chr(30);
+    E_ := 'DO';
+    Update Kd_Credit_Limit_Update A Set A.Status = 'FAILED' Where CL_Update.Customer_ID = A.Customer_ID;
+    Commit;
+    Ifsapp.Customer_Credit_Info_Api.Modify__( A_ , B_ , C_ , D_ , E_ );
+    Commit;
+    Update Kd_Credit_Limit_Update A Set A.Status = 'SUCCESS' Where CL_Update.Customer_ID = A.Customer_ID;
+  End Loop;
 End;
