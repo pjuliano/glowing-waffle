3aa2ea3c77f7b242451df8810986f0b75047cf37
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-06-13 09:15:57 -0400

    Changed Revision Text to Eng_Revision.

--- Data Requests/Manufacturing/Active Part Revisions.sql
+++ Data Requests/Manufacturing/Active Part Revisions.sql
@@ -1,11 +1,11 @@
 Select
   A.Part_No,
   Inventory_Part_Api.Get_Description(A.Contract,A.Part_No) As Part_Desc,
-  A.Revision_Text,
+  A.Eng_Revision,
   A.Eng_Chg_Level
 From
   Part_Revision A
 Where
   A.Eff_Phase_Out_Date Is Null And
-  Inventory_Part_Api.Get_Part_Status(A.Contract,A.Part_No) != 'O' And
+  Inventory_Part_Api.Get_Part_Status(A.Contract,A.Part_No) = 'A' And
   A.Contract = '100'
