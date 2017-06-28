df210ab790d90b288256721fc7ccdc7c09fa594a
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-06-28 09:22:05 -0400

    Per request from Ruthann to satisfy customer requests.

---
+++ Views/KD_Customer_Accounting_History.sql
@@ -0,0 +1,17 @@
+Create Or Replace View KD_Customer_Accounting_History As
+Select
+  A.Identity,
+  Customer_Info_Api.Get_Name(A.Identity) As Name,
+  A.Ledger_Date,
+  A.Ledger_Item_Series_Id,
+  A.Ledger_Item_ID,
+  A.Inv_Amount,
+  Ledger_Item_Util_Api.Get_Dom_Balance(A.Company,A.Identity,Customer_Info_Api.Get_Party_Type(A.Identity),To_Date(Extract(Month From A.Ledger_Date) || '/01/' || Extract(Year From A.Ledger_Date),'MM/DD/YYYY'),0) As Ledger_Month_Beginning_Balance
+From
+  Ledger_Item_Cu_Det_Qry A
+Where
+  A.Ledger_Item_Series_Id In ('II','CD','CUCHKPOA','CR') And
+  Customer_Info_Api.Get_Country(A.Identity) In ('UNITED STATES','CANADA')
+Order By
+  A.Identity,
+  A.Ledger_Date Asc;
