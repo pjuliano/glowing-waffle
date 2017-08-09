3232729c98efd2509ff5d2bd4296b330fc8e98b9
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-08-09 09:05:26 -0400

    Request from Christian.

---
+++ Data Requests/Sales/Free or Discounted Products.sql
@@ -0,0 +1,29 @@
+Select 
+  Person_Info_Api.Get_Name(A.Salesman_Code) As Rep,
+  Sum(A.Allamounts) Sales_Amount,
+  Sum(Sales_Part_Api.Get_List_Price(A.Site,A.Catalog_No) * A.Invoiced_Qty)  As List_Amount
+From 
+  KD_Sales_Data_Request A
+Where
+  A.Region_Code = 'USEC' And
+  Extract(Year From A.Invoicedate) = '2017' And
+  A.Charge_Type = 'Parts' And
+  A.Part_Product_Code = 'REGEN' And
+  A.Allamounts = 0
+Group By
+  Person_Info_Api.Get_Name(A.Salesman_Code);
+Select 
+  Person_Info_Api.Get_Name(A.Salesman_Code) As Rep,
+  Sum(A.Allamounts) Sales_Amount,
+  Sum(Sales_Part_Api.Get_List_Price(A.Site,A.Catalog_No) * A.Invoiced_Qty) As List_Amount
+From 
+  KD_Sales_Data_Request A
+Where
+  A.Region_Code = 'USEC' And
+  Extract(Year From A.Invoicedate) = '2017' And
+  A.Charge_Type = 'Parts' And
+  A.Allamounts < Sales_Part_Api.Get_List_Price(A.Site,A.Catalog_No) And
+  A.Part_Product_Code = 'REGEN' And
+  A.AllAmounts != 0
+Group By
+  Person_Info_Api.Get_Name(A.Salesman_Code)
