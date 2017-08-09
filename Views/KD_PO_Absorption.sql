37343b0485802fd4c9d722c3e6d31b4cd83e1c68
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-08-09 09:04:34 -0400

    View created for Crystal report for Mike Nealon.

---
+++ Views/KD_PO_Absorption.sql
@@ -0,0 +1,27 @@
+Create Or Replace View KD_PO_Absorption As
+Select
+  Purchase_Order_Api.Get_Vendor_No(B.Order_No) As Vendor_No,
+  Supplier_Info_Api.Get_Name(Purchase_Order_Api.Get_Vendor_No(B.Order_No)) As Vendor_Name,
+  B.Order_No,
+  To_Char(A.Date_Applied,'MM/DD/YYYY') As Date_Applied,
+  B.Transaction,
+  Case When B.Transaction != 'Receipt of Purchase Order' Then (-1 * A.Curr_Amount) Else A.Curr_Amount End As Curr_Amount,
+  A.Userid,
+  B.Part_No,
+  Sales_Part_Api.Get_Catalog_Desc(B.Contract,B.Part_No) As Description,
+  B.Quantity,
+  Case When B.Transaction != 'Receipt of Purchase Order' Then (-1 * C.Inventory_Value * B.Quantity) Else (C.Inventory_Value * B.Quantity) End As Inv_Value_Unit_Cost_X_Qty,
+  B.Lot_Batch_No,
+  B.Serial_No
+From
+  Mpccom_Accounting A,
+  Inventory_Transaction_Hist B,
+  KD_Cost_100 C
+Where
+  A.Accounting_Id = B.Accounting_Id And
+  B.Part_No = C.Part_No And
+  A.Str_Code In ('M57','M152') And
+  Extract(Year From A.Date_Applied) = Extract(Year From Sysdate) And
+  Extract(Month From A.Date_Applied) = Extract(Month From Sysdate)
+Order By
+  Purchase_Order_Api.Get_Vendor_No(B.Order_No);
