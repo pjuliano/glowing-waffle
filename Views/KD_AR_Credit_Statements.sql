d7e045aecbd6b17256f46e4f45f3395272880613
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-09-20 11:14:44 -0400

    Updated to contain detail information per Tobi's request.

--- Views/KD_AR_Credit_Statements.sql
+++ Views/KD_AR_Credit_Statements.sql
@@ -2,13 +2,19 @@ Create Or Replace View KD_AR_Credit_Statements As
 Select 
   A.Identity As Customer_No,
   B.Name,
+  A.Ledger_Date,
+  A.Due_Date,
+  A.Ledger_Item_Series_ID || A.Ledger_Item_ID As Reference,
+  A.Inv_Amount,
+  A.Open_Amount,
+  A.Currency,
   C.Address1,
   C.Address2,
   C.City,
   C.State,
   C.Zip_Code,
   C.Country,
-  Sum(A.Open_Amount) As Open_Amount
+  Sum(A.Open_Amount) As "Sum(Open_Amount)"
 From 
   Ledger_Item_Cu_Qry A, 
   Customer_Info B,
@@ -18,13 +24,19 @@ Where
   A.Identity = B.Customer_Id And
   B.Customer_Id = C.Customer_Id And
   C.Customer_Id = D.Customer_Id And
-  C.Address_ID = D.Address_ID And
+  C.Address_Id = D.Address_ID And
   D.Def_Address = 'TRUE' And
   D.Address_Type_Code = 'Document' And
   C.Country In ('UNITED STATES', 'CANADA')
 Group By
   A.Identity,
   B.Name,
+  A.Ledger_Date,
+  A.Due_Date,
+  A.Ledger_Item_Series_ID || A.Ledger_Item_ID,
+  A.Inv_Amount,
+  A.Open_Amount,
+  A.Currency,
   C.Address1,
   C.Address2,
   C.City,
@@ -32,4 +44,4 @@ Group By
   C.Zip_Code,
   C.Country
 Having
-  Sum(A.Open_Amount) < 0
+  Sum(A.Open_Amount) < 0 
