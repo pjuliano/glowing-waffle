d215ef14cbaa4f6da06537c7d5608012eb0e9055
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-10-04 12:49:52 -0400

    Using new view that captures more than just CHECK payments.

--- Views/KD_Customer_Payments.sql
+++ Views/KD_Customer_Payments.sql
@@ -1,18 +1,19 @@
-Create or Replace View KD_Customer_Payments As
+Create Or Replace View Kd_Customer_Payments As
 Select
   *
-From 
+From
 ( Select
     Identity,
     Name,
-    Extract(Month From Payment_Date) As Month,
-    Inv_Amount
+    Extract(Month From Voucher_Date) As Month,
+    Full_Curr_Amount As Inv_Amount
   From
-    Pay_Doc_Followup_Qry
+    Check_Ledger_Item
   Where
-  ((Extract(Year From Payment_Date) = Extract(Year From Sysdate)) Or
-   (Extract(Month From Payment_Date) > Extract(Month From Sysdate) And 
-    Extract(Year From Payment_Date) = Extract(Year From Sysdate) - 1)) And
-    Objstate != 'Cancelled')
+    ((Extract(Year From Payment_Date) = Extract(Year From Sysdate)) Or
+     (Extract(Month From Payment_Date) > Extract(Month From Sysdate) And 
+      Extract(Year From Payment_Date) = Extract(Year From Sysdate)-1)) And
+      Way_Id In ('CHK','CC','AMEX','VISA','MASTERCARD','SVBLB','BOMLB') And
+      Ledger_Item_Series_Id = 'CUCHECK')
 Pivot
 ( Sum(Inv_Amount) For Month In (1 As Jan,2 As Feb,3 As Mar,4 As Apr,5 As May,6 As Jun,7 As Jul,8 As Aug,9 As Sep,10 As Oct,11 As Nov,12 As Dec));
