c0bfedf0fbee895add6e52c305d397be98d4f35b
PJuliano <PJuliano@KD-PJULIANO.KeystoneDental.local>
2017-11-01 09:57:22 -0400

    Changes requested by Tobi.

--- Views/KD_Customer_Payments.sql
+++ Views/KD_Customer_Payments.sql
@@ -13,7 +13,9 @@ From
     ((Extract(Year From Payment_Date) = Extract(Year From Sysdate)) Or
      (Extract(Month From Payment_Date) > Extract(Month From Sysdate) And 
       Extract(Year From Payment_Date) = Extract(Year From Sysdate)-1)) And
-      Way_Id In ('CHK','CC','AMEX','VISA','MASTERCARD','SVBLB','BOMLB') And
-      Ledger_Item_Series_Id = 'CUCHECK')
+      Way_Id In ('CHK','CC','AMEX','VISA','MASTERCARD','SVBLB','BOMLB','WIRE','CHLB','DISCOVER','DISCOV') And
+      Objstate = 'Cashed' And
+      Company = '100' And
+      Party_Type = 'Customer')
 Pivot
 ( Sum(Inv_Amount) For Month In (1 As Jan,2 As Feb,3 As Mar,4 As Apr,5 As May,6 As Jun,7 As Jul,8 As Aug,9 As Sep,10 As Oct,11 As Nov,12 As Dec));
