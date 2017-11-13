eaad53c14b224e7402ef537c285534d81e3af1e8
Paul <pjuliano@gmail.com>
2017-11-13 13:58:13 -0500

    Just kidding!
    Don't give me what I ask for, give me what I want! I know I said Percent Achievement, but I meant Quarter to Date Achievement which is the most worthless metric ever.

--- Views/KD_QTR_QUOTA_CONTEST.sql
+++ Views/KD_QTR_QUOTA_CONTEST.sql
@@ -1,20 +1,9 @@
 Create Or Replace View KD_QTR_QUOTA_CONTEST AS
 Select
-  A.Salesman_Code,
-  Person_Info_Api.Get_Name(A.Salesman_Code) As Salesman_Name,
-  B.Qtr4 As Qtr4_Quota,
-  Sum(A.Allamounts) As Total_Sales,
-  Round(Sum(A.AllAmounts)/B.Qtr4,4) * 100 As PCT_To_Quota
+  A.Name,
+  A.Quarter_Quota_Pct,
+  A.Qtd_Quota_Pct
 From
-  Kd_Sales_Data_Request A Left Join Srrepquota B On
-    A.Salesman_Code = B.Repnumber
-Where
-  A.Charge_Type = 'Parts' And
-  A.Invoicedate Between To_Date('10/01/2017','MM/DD/YYYY') And To_Date('12/31/2017','MM/DD/YYYY') And
-  A.Corporate_Form = 'DOMDIR'
-Group By
-  A.Salesman_Code,
-  Person_Info_Api.Get_Name(A.Salesman_Code),
-  B.Qtr4
+  Kd_Svq A
 Order By
-  Round(Sum(A.AllAmounts)/B.Qtr4,4) * 100 Desc
+  A.Qtd_Quota_Pct Desc
