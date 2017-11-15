ddc501ba007bf0ede56f0e01347f48e7d9dc41be
Paul <pjuliano@gmail.com>
2017-11-15 13:17:57 -0500

    Updated View
    Removed commented sections.
    Added another section to check for AR Contact.

--- Views/KD_Customer_Check.sql
+++ Views/KD_Customer_Check.sql
@@ -1,4 +1,5 @@
 Create Or Replace Force View "IFSAPP"."KD_CUSTOMER_CHECK" ("CUSTOMER", "ISSUE") As 
+--Check to see if there is an address 99.
 Select 
   A.Customer_Id As "CUSTOMER",
   Case
@@ -18,29 +19,8 @@ Where
                 A.Customer_Id = Z.Customer_Id And
                 Z.Address_Id = '99') And
   A.Corporate_Form = 'DOMDIR'
-  --AND CUSTOMER_INFO_ADDRESS_API.IS_VALID(C.CUSTOMER_ID,F.ADDRESS_ID) = 'TRUE'
+  
 Union All
---Section 1.1 Check if Zip and Rep match.
-/*SELECT A.CUSTOMER_ID AS "CUSTOMER",
-       CASE
-         WHEN
-           CUST_ORD_CUSTOMER_API.GET_SALESMAN_CODE(A.CUSTOMER_ID) != B.REP_ID
-         THEN
-           'The rep for the zip code ' || B.ZIP_CODE || ' on Address ID ' || A.ADDRESS_ID || ' should be ' || B.REP_ID || '.'
-       END AS "ERROR"
-  FROM CUSTOMER_INFO_ADDRESS A,
-       KD_ZIP_DATA_US B,
-       CUSTOMER_INFO C
- WHERE A.ZIP_CODE = B.ZIP_CODE
-   AND A.CUSTOMER_ID = C.CUSTOMER_ID
-   AND CUST_ORD_CUSTOMER_API.GET_SALESMAN_CODE(A.CUSTOMER_ID) != B.REP_ID
-   AND CUSTOMER_INFO_ADDRESS_API.IS_VALID(A.CUSTOMER_ID,A.ADDRESS_ID) = 'TRUE'
-   AND CUST_ORD_CUSTOMER_API.GET_DATE_DEL(A.CUSTOMER_ID) IS NOT NULL
-   AND C.CORPORATE_FORM = 'DOMDIR'
-   AND A.CUSTOMER_ID NOT IN ('14295','3477','D49285','14569')
-   AND (CUSTOMER_INFO_ADDRESS_API.GET_VALID_TO(A.CUSTOMER_ID, A.ADDRESS_ID) > TRUNC(SYSDATE) OR CUSTOMER_INFO_ADDRESS_API.GET_VALID_TO(A.CUSTOMER_ID, A.ADDRESS_ID) IS NULL)
-
-UNION ALL*/
 --Section 2. Check if Salesman region is correct.
 Select 
   C.Customer_Id,
@@ -60,7 +40,7 @@ Where
   C.Corporate_Form = 'DOMDIR'
 
 Union All
-
+--Check to see if there is a non-email in the email address field.
 Select 
   A.Customer_Id, '"' || A.Value || '" is not a valid email address.' As Error
   From Customer_Info_Comm_Method A
@@ -73,12 +53,10 @@ Where
   (A.Value Not Like '%_@_%.__%' Or A.Value Like '% %' Or
   A.Value Like '%<%' Or A.Value Like '%>%' Or A.Value Like '%(%' Or
   A.Value Like '%)%' Or A.Value Like '%[%' Or A.Value Like '%]%' Or
-	--A.Value Like '%;%' Or 
   A.Value Like '%:%' Or A.Value Like '%,%' Or A.Value Like '%/%')
 
 Union All
 --Section 3. Check if Commission Receiver is NULL.
-
 Select 
   G.Customer_Id,
   'Commission receiver does not exist.'
@@ -88,15 +66,12 @@ From
 Where 
   H.Commission_Receiver Is Null And
   G.Corporate_Form = 'DOMDIR' And
-	 --AND CUST_ORD_CUSTOMER_API.GET_CUST_GRP(G.CUSTOMER_ID) != 'LAB'
-	 --AND (G.ASSOCIATION_NO IS NULL OR G.ASSOCIATION_NO = '')
-	 --AND G.ASSOCIATION_NO NOT LIKE 'N%'
   G.Customer_Id Not In ('CATEMP','TEMPLATE','5593','A29830') And
   Cust_Ord_Customer_Api.Get_Salesman_Code(G.Customer_Id) != '999'
-	 --AND CUSTOMER_INFO_ADDRESS_API.IS_VALID(C.CUSTOMER_ID,F.ADDRESS_ID) = 'TRUE'
+
 
 Union All
---Section 4. Check if Commission Receiver is correct.
+--Check if Commission Receiver is correct.
 Select 
   I.Customer_Id,
  'Commission receiver should be ' || K.Is_Rep_Id || '.'
@@ -111,9 +86,6 @@ Where
 	K.Rep_Id = L.Salesman_Code And
 	K.Is_Rep_Id != J.Commission_Receiver And
   I.Customer_Id Not In ('1871','A20529','12879','16833','1244','24129','12803','16849','A53502','A22850','17013','A68001','A76095','17899','6198','10990','14308','15381','A23610','D36013','5074')
-	 --AND CUST_ORD_CUSTOMER_API.GET_CUST_GRP(I.CUSTOMER_ID) != 'LAB'
-	 --AND (I.ASSOCIATION_NO IS NULL OR I.ASSOCIATION_NO = '')
-	 --AND CUSTOMER_INFO_ADDRESS_API.IS_VALID(C.CUSTOMER_ID,F.ADDRESS_ID) = 'TRUE'
 
 Union All
 --Check if someone used 317.
@@ -128,6 +100,7 @@ Where
   Upper(Ifsapp.Person_Info_Api.Get_Name(N.Salesman_Code)) Like 'DO NOT%' And
 	M.Customer_Id Not In ('TEMPLATE','INTL','FRTEMP','SETEMP','DETEMP','ITTEMP','BETEMP','CATEMP','UKTEMP') And
   M.Customer_Id Not Like 'DE%'
+  
 Union All
 --Check to make sure there actually is a salesman code.
 Select 
@@ -140,4 +113,21 @@ Where
   M.Customer_Id = N.Customer_Id And
   Upper(Ifsapp.Person_Info_Api.Get_Name(N.Salesman_Code)) Is Null And
 	M.Customer_Id Not In ('TEMPLATE','INTL','FRTEMP','SETEMP','DETEMP','ITTEMP','BETEMP','CATEMP','UKTEMP') And
-  M.Customer_Id Not Like 'DE%' And M.Customer_ID Not Like 'FR%' And M.Customer_ID Not Like 'IT%';
+  M.Customer_Id Not Like 'DE%' And M.Customer_Id Not Like 'FR%' And M.Customer_Id Not Like 'IT%'
+  
+Union All
+--Check to see if there is an AR Contact on the account.
+Select
+  A.Identity,
+  'Customer does not have an AR Contact.' As Issue
+From
+  Identity_Pay_Info A Left Join Identity_Invoice_Info B
+    On A.Customer_Id = B.Customer_Id
+                      Left Join Customer_Info C
+    On A.Customer_ID = C.Customer_ID
+Where
+  A.Ar_Contact Is Null And
+  B.Expire_Date Is Null And
+  A.Company = '100' And 
+  C.Corporate_Form = 'DOMDIR' And
+  A.Identity != 'TEMPLATE';
