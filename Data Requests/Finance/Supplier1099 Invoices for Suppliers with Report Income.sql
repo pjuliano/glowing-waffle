6da5dcb59c1efac93d2358824f5f142734a37912
Paul <pjuliano@gmail.com>
2017-11-27 14:30:46 -0500

    Changed year for new run.

--- Data Requests/Finance/Supplier1099 Invoices for Suppliers with Report Income.sql
+++ Data Requests/Finance/Supplier1099 Invoices for Suppliers with Report Income.sql
@@ -5,5 +5,5 @@ Select Income_Type_Per_Supplier_Api.Get_Default_Income_Type('100',A.Identity,A.P
  Where A.Party_Type_Db = 'SUPPLIER' 
    And A.Report_And_Withhold_Db = 'REPORT_INCOME'
 	 And A.Identity = B.Identity
-	 And Extract(Year From B.Invoice_Date) = Extract(Year From Sysdate) -1
+	 And Extract(Year From B.Invoice_Date) = 2017
 	 And B.State = 'PaidPosted'
