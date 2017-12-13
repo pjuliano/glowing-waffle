1aff663b67702bb90936c401e46ea2b2591c41fb
Paul <pjuliano@gmail.com>
2017-12-13 14:28:20 -0500

    Added some fields to this yearly query.

--- Data Requests/Price List Customers and Lines.sql
+++ Data Requests/Price List Customers and Lines.sql
@@ -1,16 +1,40 @@
 Select
   A.Customer_Id,
-  Sum(B.Allamounts) As Prior_Year_Sales,
-  A.Price_List_No
+  Cust_Ord_Customer_Api.Get_Salesman_Code(A.Customer_ID) As Salesman_Code,
+  Cust_Ord_Customer_Address_Api.Get_Region_Code(A.Customer_ID,Customer_Info_Address_Api.Get_Default_Address(A.Customer_Id,'Delivery')) As Region,
+  Sum(Case When Extract(Year From B.Invoicedate) = Extract(Year From Sysdate)-1
+           Then B.Allamounts
+           Else 0
+      End) As Prior_Year_Sales,
+  Sum(Case When Extract(Year From B.Invoicedate) = Extract(Year From Sysdate)
+           Then B.Allamounts
+           Else 0
+      End) As Current_Year_Sales,
+  Sum(Case When Extract(Year From B.Invoicedate) = Extract(Year From Sysdate)
+           Then B.Allamounts
+           Else 0
+      End) -
+  Sum(Case When Extract(Year From B.Invoicedate) = Extract(Year From Sysdate)-1
+           Then B.Allamounts
+           Else 0
+      End) As Delta,  
+  A.Price_List_No,
+  Sales_Price_List_Api.Get_Valid_To_Date(A.Price_List_No) As Expiration_Date
 From
   Customer_Pricelist_Ent A Left Join Kd_Sales_Data_Request B
     On A.Customer_ID = B.Customer_No
 Where
-  Extract(Year From B.Invoicedate) = Extract(Year From Sysdate) - 1 And
+  Extract(Year From B.Invoicedate) >= Extract(Year From Sysdate)-1 And
   B.Charge_Type = 'Parts'
 Group By
   A.Customer_Id,
-  A.Price_List_No;
+  Cust_Ord_Customer_Api.Get_Salesman_Code(A.Customer_ID),
+  Cust_Ord_Customer_Address_Api.Get_District_Code(A.Customer_ID,Customer_Info_Address_Api.Get_Default_Address(A.Customer_Id,'Delivery')),
+  A.Price_List_No
+Order By
+  Cust_Ord_Customer_Address_Api.Get_District_Code(A.Customer_ID,Customer_Info_Address_Api.Get_Default_Address(A.Customer_Id,'Delivery')),
+  Cust_Ord_Customer_Api.Get_Salesman_Code(A.Customer_ID),
+  A.Customer_ID;
   
 With Latest_Price_List_Line As
   (
@@ -41,14 +65,19 @@ Select
             100 - (B.Sales_Price / B.Base_Price) * 100
           Else
             100
-        End,5) As Effective_Discount_Pct
+        End,5) As Effective_Discount_Pct,
+  D.Inventory_Value,
+  Round((B.Sales_Price - D.Inventory_Value) / Nullif(B.Sales_Price,0),4) * 100 As Margin
 From
   Sales_Price_List_Part B,
-  Latest_Price_List_Line C
+  Latest_Price_List_Line C Left Join KD_Cost_100 D
+    On C.Catalog_No = D.Part_No
 Where
   B.Valid_From_Date = C.Latest_Date And
   B.Price_List_No = C.Price_List_No And 
   B.Catalog_No = C.Catalog_No And
   B.State = 'Active' And
-  Sales_Part_Api.Get_Activeind('100',B.Catalog_No) != 'Inactive Part' And
-  B.Price_List_No In ('N1013SI','N1013T','10% W' || chr(38) || 'H');
+  Sales_Part_Api.Get_Activeind('100',B.Catalog_No) != 'Inactive Part'
+Order By
+  B.Price_List_No,
+  B.Catalog_No
