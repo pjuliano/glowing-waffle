aafdf43ec5c1cac13fb48206676e5db9200a4c98
Paul <pjuliano@gmail.com>
2018-01-10 15:53:42 -0500

    Error Fixing
    The $$3 was being added into nonsterile character values, which threw off the mod43 calculation and returned the wrong check character. I accounted for this with a decode and an NVL.

--- Views/KD_UDIs.sql
+++ Views/KD_UDIs.sql
@@ -9,14 +9,14 @@ Select
   Decode(Expiration_Date,Null,'','$$3') || --Date/lot reference identifier
   To_Char(Expiration_Date,'YYMMDD') || --Expiration in YYMMDD
   Substr(Regexp_Replace(Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18) || --First 18 characters of lot/batch number stripped of non-alphanumeric characters
-  (Select Character From KD_UDI_Chars Where Value = 
+  (Select Character From KD_UDI_Chars Where Value = --Find check character
   Mod(
-      (Select Value From Kd_Udi_Chars Where Character = '+') + 
-      (Select Value From Kd_Udi_Chars Where Character = Substr('D768',1,1)) + 
-      (Select Value From Kd_Udi_Chars Where Character = Substr('D768',2,1)) +
-      (Select Value From Kd_Udi_Chars Where Character = Substr('D768',3,1)) + 
-      (Select Value From Kd_Udi_Chars Where Character = Substr('D768',4,1)) +
-      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),1,1)),0) +
+      (Select Value From Kd_Udi_Chars Where Character = '+') + --Data Identifier
+      (Select Value From Kd_Udi_Chars Where Character = Substr('D768',1,1)) + --MFG Code
+      (Select Value From Kd_Udi_Chars Where Character = Substr('D768',2,1)) + --MFG Code
+      (Select Value From Kd_Udi_Chars Where Character = Substr('D768',3,1)) + --MFG Code
+      (Select Value From Kd_Udi_Chars Where Character = Substr('D768',4,1)) + --MFG Code
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),1,1)),0) + --Part Number characters 1-18 values
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),2,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),3,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),4,1)),0) +
@@ -34,18 +34,18 @@ Select
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),16,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),17,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),18,1)),0) + 
-      (Select Value From Kd_Udi_Chars Where Character = Substr('0',1,1)) +
-      (Select Value From Kd_Udi_Chars Where Character = Substr('/',1,1)) +
-      (Select Value From Kd_Udi_Chars Where Character = Substr('$$3',1,1)) +
-      (Select Value From Kd_Udi_Chars Where Character = Substr('$$3',2,1)) +
-      (Select Value From Kd_Udi_Chars Where Character = Substr('$$3',3,1)) +
-      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),1,1)),0) +
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr('0',1,1)),0) + --UoM
+      (Select Value From Kd_Udi_Chars Where Character = Substr('/',1,1)) + --Data Delimiter 
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'','$$3'),1,1)),0) + --Date/Lot reference identifier (0 if Expiration Date is Null, indicating a non-sterile part)
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'','$$3'),2,1)),0) +
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'','$$3'),3,1)),0) +
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),1,1)),0) + --Expiration Date in YYMMDD
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),2,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),3,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),4,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),5,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),6,1)),0) +
-      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,1)),0) +
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,1)),0) + --Lot Number characters 1-18 values
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Lot_Batch_No,'[^a-zA-Z0-9]+',''),2,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Lot_Batch_No,'[^a-zA-Z0-9]+',''),3,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Lot_Batch_No,'[^a-zA-Z0-9]+',''),4,1)),0) +
@@ -67,7 +67,11 @@ Select
   Expiration_Date,
   Lot_Batch_No
 From
-  Inventory_Transaction_Hist
+  Inventory_transaction_Hist
 Where
-  Transaction_Code = 'OESHIP' And Lot_Batch_No != '*' And
-  Expiration_Date >= To_Date('01/01/2015','MM/DD/YYYY')
+  Transaction_Code = 'OESHIP' And
+  Lot_Batch_No != '*'
+Group By
+  Part_No,
+  Expiration_Date,
+  Lot_Batch_No
