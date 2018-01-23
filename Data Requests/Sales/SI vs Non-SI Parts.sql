d5c24666934685c4d97da649d6c7e8f9644c14fb
Paul <pjuliano@gmail.com>
2018-01-23 11:47:49 -0500

    Jason Hyland's data request.

--- Data Requests/Sales/SI vs Non-SI Parts.sql
+++ Data Requests/Sales/SI vs Non-SI Parts.sql
@@ -1,20 +1,24 @@
+Select 
+  * 
+From 
+  (
 Select
   *
 From
   (
   Select
     A.Association_No,
-    Case When A.Part_Product_Family In ('CALFO','CALMA','CAPSE','CYTOP','EXHEX','EXORL','OCT','PRSFT','TEFGE','TRINX','ZMAX') Then 'SI'
-         Else 'NOT SI'
-    End As Company,
+    A.Part_Product_Family,
     A.AllAmounts
   From
     KD_Sales_Data_Request A
   Where
     A.Association_No Like 'N1%' And
     Extract(Year From A.Invoicedate) = 2017 And
-    A.Charge_Type = 'Parts'
+    A.Charge_Type = 'Parts' And
+    A.Part_Product_Family In ('COMM','GNSIS','PRIMA','RENOV','RESTO','STAGE','SUST','TEFGE','XP1','TRINX','EXHEX','EXORL','OCT','ZMAX','LODI','OTMED','PRMA+','PCOMM','TLMAX')
   )
 Pivot
   (
-  Sum(AllAmounts) For Company In ('SI' As "SI",'NOT SI' As "NOT SI"))
+  Sum(AllAmounts) For Part_Product_Family In ('COMM','GNSIS','PRIMA','RENOV','RESTO','STAGE','SUST','TEFGE','XP1','TRINX','EXHEX','EXORL','OCT','ZMAX','LODI','OTMED','PRMA+','PCOMM','TLMAX'))
+ ) Sub1 Order By 1 Asc
