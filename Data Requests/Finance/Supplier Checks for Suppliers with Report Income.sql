1b472c4cf16ec001061a0c338a00ce97d15018fb
Paul <pjuliano@gmail.com>
2018-01-24 14:40:33 -0500

    Added complete addresses for Leslie.

--- Data Requests/Finance/Supplier Checks for Suppliers with Report Income.sql
+++ Data Requests/Finance/Supplier Checks for Suppliers with Report Income.sql
@@ -1,7 +1,11 @@
---Include Full Address!
 Select
   Identity_Invoice_Info_Api.Get_Vat_No(A.Company,A.Identity,A.Party_Type) As Tax_Id,
-  A.*
+  A.*,
+  Supplier_Info_Address_Api.Get_Address1(A.Identity,A.Address_Id) As Address1,
+  Supplier_Info_Address_Api.Get_Address2(A.Identity,A.Address_Id) As Address2,
+  Supplier_Info_Address_Api.Get_City(A.Identity,A.Address_Id) As City,
+  Supplier_Info_Address_Api.Get_State(A.Identity,A.Address_Id) As State,
+  Supplier_Info_Address_Api.Get_Zip_Code(A.Identity,A.Address_Id) As Zip_Code
 From
   Check_Ledger_Item A
 Where
