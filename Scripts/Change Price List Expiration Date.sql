872a27e506cb933ad402b898a463421c1633900e
Paul <pjuliano@gmail.com>
2018-01-30 09:53:25 -0500

    No one replied so I had to push all price lists out to 2019.

--- Scripts/Change Price List Expiration Date.sql
+++ Scripts/Change Price List Expiration Date.sql
@@ -16,7 +16,7 @@ Begin
     A_ := '';
     B_ := Cursor.Objid;
     C_ := Cursor.Objversion;
-    D_ := 'VALID_TO_DATE'||Chr(31)||'2018-01-31-00.00.00'||Chr(30);
+    D_ := 'VALID_TO_DATE'||Chr(31)||'2019-01-31-00.00.00'||Chr(30);
     E_ := 'DO';
     Ifsapp.Sales_Price_List_Api.Modify__( A_ , B_ , C_ , D_ , E_ );
   End Loop;
