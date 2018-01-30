39330294bb03219c5f59d9353afa8c622e9bfac6
Paul <pjuliano@gmail.com>
2018-01-30 09:53:53 -0500

    Data Identifier for parts with null expiration dates should be $$7 not null!

--- Views/KD_UDIs.sql
+++ Views/KD_UDIs.sql
@@ -6,7 +6,7 @@ Select
   Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),1,18) || --First 18 characters of part number stripped of non-alphanumeric characters
   '0' || --Unit of measure
   '/' || --Data delimiter
-  Decode(Expiration_Date,Null,'','$$3') || --Date/lot reference identifier
+  Decode(Expiration_Date,Null,'$$7','$$3') || --Date/lot reference identifier
   To_Char(Expiration_Date,'YYMMDD') || --Expiration in YYMMDD
   Substr(Regexp_Replace(Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18) || --First 18 characters of lot/batch number stripped of non-alphanumeric characters
   (Select Character From KD_UDI_Chars Where Value = --Find check character
@@ -36,9 +36,9 @@ Select
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Regexp_Replace(Part_No,'[^a-zA-Z0-9]+',''),18,1)),0) + 
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr('0',1,1)),0) + --UoM
       (Select Value From Kd_Udi_Chars Where Character = Substr('/',1,1)) + --Data Delimiter 
-      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'','$$3'),1,1)),0) + --Date/Lot reference identifier (0 if Expiration Date is Null, indicating a non-sterile part)
-      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'','$$3'),2,1)),0) +
-      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'','$$3'),3,1)),0) +
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'$$7','$$3'),1,1)),0) + --Date/Lot reference identifier (0 if Expiration Date is Null, indicating a non-sterile part)
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'$$7','$$3'),2,1)),0) +
+      Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(Decode(Expiration_Date,Null,'$$7','$$3'),3,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),1,1)),0) + --Expiration Date in YYMMDD
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),2,1)),0) +
       Nvl((Select Value From Kd_Udi_Chars Where Character = Substr(To_Char(Expiration_Date,'YYMMDD'),3,1)),0) +
