b73a53b8d615c3315bc2c5e82bb6635fb0e788b9
Paul <pjuliano@gmail.com>
2018-04-30 09:39:50 -0400

    Brandon Request

--- Scripts/Inventory Move.sql
+++ Scripts/Inventory Move.sql
@@ -19,31 +19,47 @@ Declare
    R_ Varchar2(32000) := 'N'; --p17
    S_ Float := Null; --p18
 Begin
-  For Inv In (Select * From Kd_Inv_Move A Where A.Comments Not In ('FAILED','MOVED') or A.Comments Is Null)
+  For Inv In (Select
+                A.Contract,
+                A.Part_No,
+                A.Configuration_Id As Config_Id,
+                'CLEANROOMCA' As Move_From,
+                A.Lot_Batch_No,
+                A.Configuration_Id,
+                A.Eng_Chg_Level as Rev_No,
+                A.Serial_No,
+                '0',
+                To_Date(A.Expiration_Date,'MM/DD/YYYY') As Expiration_Date,
+                A.Contract As Contract1,
+                'CLEANROOMCH' As Move_To,
+                'Move to inventory',
+                A.Qty_Onhand - A.Qty_Reserved As Qty_Available,
+                '',
+                'N'
+              From
+                Inventory_Part_In_Stock A
+              Where
+                Location_No = 'CLEANROOMCH')
   Loop
-    Update Kd_Inv_Move A Set A.Comments = 'FAILED' Where Inv.Part_No = A.Part_No And Inv.Lot_Batch_No = A.Lot_Batch_No And Inv.From_Location_No = A.From_Location_No;
-    Commit;
     A_ := Null;
     B_ := Null;
-    C_ := Inv.From_Site;
+    C_ := Inv.Contract;
     D_ := Inv.Part_No;
-    E_ := Inv.Config_Id;
-    F_ := Inv.From_Location_No;
+    E_ := Inv.CONFIG_ID;
+    F_ := Inv.Move_From;
     G_ := Inv.Lot_Batch_No;
-    H_ := Inv.Config_Id;
+    H_ := Inv.Configuration_Id;
     I_ := Inv.Rev_No;
     J_ := Inv.Serial_No;
     K_ := 0;
-    L_ := To_Date(Inv.Expiration_Date,'MM/DD/YYYY');
-    M_ := Inv.To_Site;
-    N_ := Inv.To_Location;
+    L_ := Inv.Expiration_Date;
+    M_ := Inv.Contract;
+    N_ := Inv.Move_To;
     O_ := 'Move to inventory';
-    P_ := Inv.On_Hand_Qty;
+    P_ := Inv.Qty_Available;
     Q_ := '';
     R_ := 'N';
     S_ := Null;
     Ifsapp.Inventory_Part_In_Stock_Api.Move_Part( A_ , B_ , C_ , D_ , E_ , F_ , G_ , H_ , I_ , J_ , K_ , L_ , M_ , N_ , O_ , P_ , 0, Q_ , Null, Null, Null, Null, Null, R_ , Null, S_ );
-    Update KD_Inv_Move A Set A.Comments = 'MOVED' Where Inv.Part_No = A.Part_No And Inv.Lot_Batch_No = A.Lot_Batch_No And Inv.From_Location_No = A.From_Location_No;
-    Commit;
   End Loop;
 End;
