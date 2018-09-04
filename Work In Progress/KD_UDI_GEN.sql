3adff7616cc84fc8aba0bf9cd1f46347a467e570
Juliano <PJuliano@KeystoneDental.com>
2018-09-04 11:44:38 -0400

    Updates
    Worked with Jon to figure out process a bit better and add internal variable to hold the new exp. date.

--- Work In Progress/KD_UDI_GEN.sql
+++ Work In Progress/KD_UDI_GEN.sql
@@ -2,80 +2,90 @@ Create Or Replace Function Kd_Udi_Gen (V_Part_No In Varchar2,
                                        V_Production_Line In Varchar2, 
                                        V_Exp_Date In Date,
                                        V_Lot_Batch_No In Varchar2) 
-    Return Varchar2 As
+    Return Varchar2 Is
+    V_Exp_Date_Plus Date;
 Begin
+    V_Exp_Date_Plus := To_Date(Case 
+                            When Extract(Month From V_Exp_Date) In (1,3,5,7,8,10,12) Then
+                            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '31'
+                            When Extract(Month From V_Exp_Date) In (4,6,9,11) Then
+                            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '30'
+                            When Extract(Month From V_Exp_Date) = 2 And Mod(Extract(Year From V_Exp_Date)+5,4) = 0 And Mod(Extract(Year From V_Exp_Date)+5,100) != 0 Then
+                            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '29'
+                            When Extract(Month From V_Exp_Date) = 2 And Mod(Extract(Year From V_Exp_Date)+5,4) = 0 And Mod(Extract(Year From V_Exp_Date)+5,100) = 0 And Mod(Extract(Year From V_Exp_Date)+5,400) = 0 Then
+                            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '29'
+                            When Extract(Month From V_Exp_Date) = 2 And Mod(Extract(Year From V_Exp_Date)+5,4) = 0 And Mod(Extract(Year From V_Exp_Date)+5,100) = 0 And Mod(Extract(Year From V_Exp_Date)+5,400) != 0 Then
+                            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '28'
+                            When Extract(Month From V_Exp_Date) = 2 And Mod(Extract(Year From V_Exp_Date)+5,4) != 0 Then
+                            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '28'
+                          End,'YYMMDD');
     Return
+    Case 
+        When V_Exp_Date Is Null 
+        Then 'SO Must Be Closed.'
+        When V_Exp_Date < To_Date('09/01/2018','MM/DD/YYYY')
+        Then 'Before Inception Date.'
+        When V_Production_Line Not Like 'Sterile%' And V_Production_Line Not Like 'Non%' Then 'Check Production Line.'
+        Else
     Case 
         When V_Production_Line Like 'Sterile%' Then
         '+D768' || --Data Identifier and MFG Code
         Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18) || --First 18 characters of part number stripped of non-alphanumeric characters
         '0/$$3' || --UoM, Data Delimeter, and Date Reference Identifier
-        Case 
-            When Extract(Month From V_Exp_Date) In (1,3,5,7,8,10,12) Then
-            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '31'
-            When Extract(Month From V_Exp_Date) In (4,6,9,11) Then
-            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '30'
-            When Extract(Month From V_Exp_Date) = 2 And Mod(Extract(Year From V_Exp_Date)+5,4) = 0 And Mod(Extract(Year From V_Exp_Date)+5,100) != 0 Then
-            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '29'
-            When Extract(Month From V_Exp_Date) = 2 And Mod(Extract(Year From V_Exp_Date)+5,4) = 0 And Mod(Extract(Year From V_Exp_Date)+5,100) = 0 And Mod(Extract(Year From V_Exp_Date)+5,400) = 0 Then
-            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '29'
-            When Extract(Month From V_Exp_Date) = 2 And Mod(Extract(Year From V_Exp_Date)+5,4) = 0 And Mod(Extract(Year From V_Exp_Date)+5,100) = 0 And Mod(Extract(Year From V_Exp_Date)+5,400) != 0 Then
-            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '28'
-            When Extract(Month From V_Exp_Date) = 2 And Mod(Extract(Year From V_Exp_Date)+5,4) != 0 Then
-            To_Char(V_Exp_Date,'yy') + 5 || To_Char(V_Exp_Date,'mm') || '28'
-        End || --Expiration Date in YYMMDD format
+        To_Char(V_Exp_Date_Plus,'YYMMDD') || --Expiration Date in YYMMDD format
         Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18) || --First 18 characters of lot/batch stripped of non-alphanumeric characters
         Kd_Udi_Check_Char(Mod(Kd_Udi_Char_Value('+D768',1) +
                               Kd_Udi_Char_Value('+D768',2) +
                               Kd_Udi_Char_Value('+D768',3) +
                               Kd_Udi_Char_Value('+D768',4) +
-                              Kd_Udi_Char_Value(V_Part_No,1) +
-                              Kd_Udi_Char_Value(V_Part_No,2) +
-                              Kd_Udi_Char_Value(V_Part_No,3) +
-                              Kd_Udi_Char_Value(V_Part_No,4) +
-                              Kd_Udi_Char_Value(V_Part_No,5) +
-                              Kd_Udi_Char_Value(V_Part_No,6) +
-                              Kd_Udi_Char_Value(V_Part_No,7) +
-                              Kd_Udi_Char_Value(V_Part_No,8) +
-                              Kd_Udi_Char_Value(V_Part_No,9) +
-                              Kd_Udi_Char_Value(V_Part_No,10) +
-                              Kd_Udi_Char_Value(V_Part_No,11) +
-                              Kd_Udi_Char_Value(V_Part_No,12) +
-                              Kd_Udi_Char_Value(V_Part_No,13) +
-                              Kd_Udi_Char_Value(V_Part_No,14) +
-                              Kd_Udi_Char_Value(V_Part_No,15) +
-                              Kd_Udi_Char_Value(V_Part_No,16) +
-                              Kd_Udi_Char_Value(V_Part_No,17) +
-                              Kd_Udi_Char_Value(V_Part_No,18) +
+                              Kd_Udi_Char_Value('+D768',5) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),1) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),2) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),3) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),4) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),5) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),6) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),7) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),8) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),9) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),10) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),11) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),12) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),13) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),14) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),15) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),16) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),17) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),18) +
                               Kd_Udi_Char_Value('0/$$3',1) +
                               Kd_Udi_Char_Value('0/$$3',2) +
                               Kd_Udi_Char_Value('0/$$3',3) +
                               Kd_Udi_Char_Value('0/$$3',4) +
                               Kd_Udi_Char_Value('0/$$3',5) +
-                              Kd_Udi_Char_Value(To_Char(V_Exp_Date,'YYMMDD'),1) +
-                              Kd_Udi_Char_Value(To_Char(V_Exp_Date,'YYMMDD'),2) +
-                              Kd_Udi_Char_Value(To_Char(V_Exp_Date,'YYMMDD'),3) +
-                              Kd_Udi_Char_Value(To_Char(V_Exp_Date,'YYMMDD'),4) +
-                              Kd_Udi_Char_Value(To_Char(V_Exp_Date,'YYMMDD'),5) +
-                              Kd_Udi_Char_Value(To_Char(V_Exp_Date,'YYMMDD'),6) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,1) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,2) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,3) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,4) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,5) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,6) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,7) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,8) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,9) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,10) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,11) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,12) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,13) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,14) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,15) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,16) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,17) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,18),43))
+                              Kd_Udi_Char_Value(To_Char(V_Exp_Date_Plus,'YYMMDD'),1) +
+                              Kd_Udi_Char_Value(To_Char(V_Exp_Date_Plus,'YYMMDD'),2) +
+                              Kd_Udi_Char_Value(To_Char(V_Exp_Date_Plus,'YYMMDD'),3) +
+                              Kd_Udi_Char_Value(To_Char(V_Exp_Date_Plus,'YYMMDD'),4) +
+                              Kd_Udi_Char_Value(To_Char(V_Exp_Date_Plus,'YYMMDD'),5) +
+                              Kd_Udi_Char_Value(To_Char(V_Exp_Date_Plus,'YYMMDD'),6) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),1) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),2) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),3) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),4) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),5) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),6) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),7) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),8) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),9) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),10) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),11) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),12) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),13) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),14) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),15) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),16) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),17) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),18),43))
         When V_Production_Line Like 'Non%' Then
         '+D768' || --Data Identifier and MFG Code
         Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18) || --First 18 characters of part number stripped of non-alphanumeric characters
@@ -84,48 +94,50 @@ Begin
         Kd_Udi_Check_Char(Mod(Kd_Udi_Char_Value('+D768',1) +
                               Kd_Udi_Char_Value('+D768',2) +
                               Kd_Udi_Char_Value('+D768',3) +
-                              Kd_Udi_Char_Value('+D768',4) +
-                              Kd_Udi_Char_Value(V_Part_No,1) +
-                              Kd_Udi_Char_Value(V_Part_No,2) +
-                              Kd_Udi_Char_Value(V_Part_No,3) +
-                              Kd_Udi_Char_Value(V_Part_No,4) +
-                              Kd_Udi_Char_Value(V_Part_No,5) +
-                              Kd_Udi_Char_Value(V_Part_No,6) +
-                              Kd_Udi_Char_Value(V_Part_No,7) +
-                              Kd_Udi_Char_Value(V_Part_No,8) +
-                              Kd_Udi_Char_Value(V_Part_No,9) +
-                              Kd_Udi_Char_Value(V_Part_No,10) +
-                              Kd_Udi_Char_Value(V_Part_No,11) +
-                              Kd_Udi_Char_Value(V_Part_No,12) +
-                              Kd_Udi_Char_Value(V_Part_No,13) +
-                              Kd_Udi_Char_Value(V_Part_No,14) +
-                              Kd_Udi_Char_Value(V_Part_No,15) +
-                              Kd_Udi_Char_Value(V_Part_No,16) +
-                              Kd_Udi_Char_Value(V_Part_No,17) +
-                              Kd_Udi_Char_Value(V_Part_No,18) +
+                              Kd_Udi_Char_Value('+D768',4) + 
+                              Kd_Udi_Char_Value('+D768',5) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),1) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),2) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),3) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),4) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),5) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),6) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),7) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),8) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),9) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),10) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),11) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),12) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),13) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),14) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),15) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),16) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),17) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Part_No,'[^a-zA-Z0-9]+',''),1,18),18) +
                               Kd_Udi_Char_Value('0/$$7',1) +
                               Kd_Udi_Char_Value('0/$$7',2) +
                               Kd_Udi_Char_Value('0/$$7',3) +
                               Kd_Udi_Char_Value('0/$$7',4) +
                               Kd_Udi_Char_Value('0/$$7',5) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,1) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,2) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,3) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,4) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,5) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,6) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,7) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,8) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,9) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,10) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,11) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,12) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,13) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,14) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,15) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,16) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,17) +
-                              Kd_Udi_Char_Value(V_Lot_Batch_No,18),43))
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),1) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),2) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),3) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),4) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),5) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),6) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),7) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),8) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),9) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),10) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),11) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),12) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),13) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),14) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),15) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),16) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),17) +
+                              Kd_Udi_Char_Value(Substr(Regexp_Replace(V_Lot_Batch_No,'[^a-zA-Z0-9]+',''),1,18),18),43))
         
+    End
     End;
 End KD_UDI_Gen;
