14b7a3115041cf8ce8719a6b970f5be5f88bf61e
Juliano <PJuliano@KeystoneDental.com>
2018-11-09 09:19:59 -0500

    Update Work In Progress/KD_ASP.sql

--- Work In Progress/KD_ASP.sql
+++ Work In Progress/KD_ASP.sql
@@ -1,7 +1,8 @@
+Create Or Replace View KD_ASP As 
 With CY_ASP As (
 Select
+    A.Region_Code,
     A.Salesman_Code,
-    Person_Info_Api.Get_Name(A.Salesman_Code) Salesman_Name,
     Round(Sum(Case When Part_Product_Family = 'PRIMA' Then
                    A.AllAmounts
                    Else 0
@@ -157,16 +158,15 @@ Where
     A.Order_No Is Null) And
    (A.Market_Code != 'PREPOST' Or A.Market_Code Is Null) And
     A.Invoice_ID != 'CR1001802096' --20180904 Invoice is stuck not posted and cannot be deleted.
-Group By
-    A.Salesman_Code,
-    Person_Info_Api.Get_Name(A.Salesman_Code)
+Group By 
+    Rollup(A.Region_Code,A.Salesman_Code)
 Order By 
     A.Salesman_Code),
     
 PY_ASP AS (
 Select
+    A.Region_Code,
     A.Salesman_Code,
-    Person_Info_Api.Get_Name(A.Salesman_Code) Salesman_Name,
     Round(Sum(Case When Part_Product_Family = 'PRIMA' Then
                    A.AllAmounts
                    Else 0
@@ -323,60 +323,60 @@ Where
    (A.Market_Code != 'PREPOST' Or A.Market_Code Is Null) And
     A.Invoice_ID != 'CR1001802096' --20180904 Invoice is stuck not posted and cannot be deleted.
 Group By
-    A.Salesman_Code,
-    Person_Info_Api.Get_Name(A.Salesman_Code)
+    Rollup(A.Region_Code,A.Salesman_Code)
 Order By 
     A.Salesman_Code)
 
 Select
-    C.Region,
+    A.Region_Code,
     A.Salesman_Code,
-    A.Salesman_Name,
-    A."CY_PRIMA_ASP",
-    B."PY_PRIMA_ASP",
-    A."CY_PRIMA_ASP" - B."PY_PRIMA_ASP" "PRIMA_ASP_DELTA",
-    A."CY_PRMA+_ASP",
-    B."PY_PRMA+_ASP",
-    A."CY_PRMA+_ASP" - B."PY_PRMA+_ASP" "PRMA+_ASP_DELTA",
-    A."CY_GNSIS_ASP",
-    B."PY_GNSIS_ASP",
-    A."CY_GNSIS_ASP" - B."PY_GNSIS_ASP" "GNSIS_ASP_DELTA",
-    A."CY_TLMAX_ASP",
-    B."PY_TLMAX_ASP",
-    A."CY_TLMAX_ASP" - B."PY_TLMAX_ASP" "TLMAX_ASP_DELTA",
-    A."CY_PCOMM_ASP",
-    B."PY_PCOMM_ASP",
-    A."CY_PCOMM_ASP" - B."PY_PCOMM_ASP" "PCOMM_ASP_DELTA",
-    A."CY_COMM_ASP",
-    B."PY_COMM_ASP",
-    A."CY_COMM_ASP" - B."PY_PCOMM_ASP" "COMM_ASP_DELTA",
-    A."CY_BVINE_ASP",
-    B."PY_BVINE_ASP",
-    A."CY_BVINE_ASP" - B."PY_BVINE_ASP" "BVINE_ASP_DELTA",
-    A."CY_CONNX_ASP",
-    B."PY_CONNX_ASP",
-    A."CY_CONNX_ASP" - B."PY_CONNX_ASP" "CONNX_ASP_DELTA",
-    A."CY_CYTOP_ASP",
-    B."PY_CYTOP_ASP",
-    A."CY_CYTOP_ASP" - B."PY_CYTOP_ASP" "CYTOP_ASP_DELTA",
-    A."CY_DYNAB_ASP",
-    B."PY_DYNAB_ASP",
-    A."CY_DYNAB_ASP" - B."PY_DYNAB_ASP" "DYNAB_ASP_DELTA",
-    A."CY_DYNAG_ASP",
-    B."PY_DYNAG_ASP",
-    A."CY_DYNAG_ASP" - B."PY_DYNAG_ASP" "DYNAG_ASP_DELTA",
-    A."CY_DYNAM_ASP",
-    B."PY_DYNAM_ASP",
-    A."CY_DYNAM_ASP" - B."PY_DYNAM_ASP" "DYNAM_ASP_DELTA",
-    A."CY_SYNTH_ASP",
-    B."PY_SYNTH_ASP",
-    A."CY_SYNTH_ASP" - B."PY_SYNTH_ASP" "SYNTH_ASP_DELTA"
+    Person_Info_Api.Get_Name(A.Salesman_Code) Salesman_Name,
+    Nvl(A."CY_PRIMA_ASP",0) "CY_PRIMA_ASP",
+    Nvl(B."PY_PRIMA_ASP",0) "PY_PRIMA_ASP",
+    Nvl(A."CY_PRIMA_ASP" - B."PY_PRIMA_ASP",0) "PRIMA_ASP_DELTA",
+    Nvl(A."CY_PRMA+_ASP",0) "CY_PRMA+_ASP",
+    Nvl(B."PY_PRMA+_ASP",0) "PY_PRMA+_ASP",
+    Nvl(A."CY_PRMA+_ASP" - B."PY_PRMA+_ASP",0) "PRMA+_ASP_DELTA",
+    Nvl(A."CY_GNSIS_ASP",0) "CY_GNSIS_ASP",
+    Nvl(B."PY_GNSIS_ASP",0) "PY_GNSIS_ASP",
+    Nvl(A."CY_GNSIS_ASP" - B."PY_GNSIS_ASP",0) "GNSIS_ASP_DELTA",
+    Nvl(A."CY_TLMAX_ASP",0) "CY_TLMAX_ASP",
+    Nvl(B."PY_TLMAX_ASP",0) "PY_TLMAX_ASP",
+    Nvl(A."CY_TLMAX_ASP" - B."PY_TLMAX_ASP",0) "TLMAX_ASP_DELTA",
+    Nvl(A."CY_PCOMM_ASP",0) "CY_PCOMM_ASP",
+    Nvl(B."PY_PCOMM_ASP",0) "PY_PCOMM_ASP",
+    Nvl(A."CY_PCOMM_ASP" - B."PY_PCOMM_ASP",0) "PCOMM_ASP_DELTA",
+    Nvl(A."CY_COMM_ASP",0) "CY_COMM_ASP",
+    Nvl(B."PY_COMM_ASP",0) "PY_COMM_ASP",
+    Nvl(A."CY_COMM_ASP" - B."PY_COMM_ASP",0) "COMM_ASP_DELTA",
+    Nvl(A."CY_BVINE_ASP",0) "CY_BVINE_ASP",
+    Nvl(B."PY_BVINE_ASP",0) "PY_BVINE_ASP",
+    Nvl(A."CY_BVINE_ASP" - B."PY_BVINE_ASP",0) "BVINE_ASP_DELTA",
+    Nvl(A."CY_CONNX_ASP",0) "CY_CONNX_ASP",
+    Nvl(B."PY_CONNX_ASP",0) "PY_CONNX_ASP",
+    Nvl(A."CY_CONNX_ASP" - B."PY_CONNX_ASP",0) "CONNX_ASP_DELTA",
+    Nvl(A."CY_CYTOP_ASP",0) "CY_CYTOP_ASP",
+    Nvl(B."PY_CYTOP_ASP",0) "PY_CYTOP_ASP",
+    Nvl(A."CY_CYTOP_ASP" - B."PY_CYTOP_ASP",0) "CYTOP_ASP_DELTA",
+    Nvl(A."CY_DYNAB_ASP",0) "CY_DYNAB_ASP",
+    Nvl(B."PY_DYNAB_ASP",0) "PY_DYNAB_ASP",
+    Nvl(A."CY_DYNAB_ASP" - B."PY_DYNAB_ASP",0) "DYNAB_ASP_DELTA",
+    Nvl(A."CY_DYNAG_ASP",0) "CY_DYNAG_ASP",
+    Nvl(B."PY_DYNAG_ASP",0) "PY_DYNAG_ASP",
+    Nvl(A."CY_DYNAG_ASP" - B."PY_DYNAG_ASP",0) "DYNAG_ASP_DELTA",
+    Nvl(A."CY_DYNAM_ASP",0) "CY_DYNAM_ASP",
+    Nvl(B."PY_DYNAM_ASP",0) "PY_DYNAM_ASP",
+    Nvl(A."CY_DYNAM_ASP" - B."PY_DYNAM_ASP",0) "DYNAM_ASP_DELTA",
+    Nvl(A."CY_SYNTH_ASP",0) "CY_SYNTH_ASP",
+    Nvl(B."PY_SYNTH_ASP",0) "PY_SYNTH_ASP",
+    Nvl(A."CY_SYNTH_ASP" - B."PY_SYNTH_ASP",0) "SYNTH_ASP_DELTA"
 From
-    CY_ASP A Left Join Srrepquota C
-        On A.Salesman_Code = C.Repnumber,
+    CY_ASP A,
     PY_ASP B
 Where
-    A.Salesman_Code = B.Salesman_Code
+    (A.Salesman_Code = B.Salesman_Code And A.Region_Code = B.Region_Code) Or 
+    (A.Salesman_Code Is Null And B.Salesman_Code Is Null And A.Region_Code = B.Region_Code) Or
+    (A.Salesman_Code Is Null and B.Salesman_Code IS Null And A.Region_Code Is Null and B.Region_Code IS Null)
 Order By
-    C.Region,
+    A.Region_Code,
     A.Salesman_Code
